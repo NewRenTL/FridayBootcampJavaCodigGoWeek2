2638d277e12298915882d5c3dff06dc30f7740f8
diego.bustamante@utec.edu.pe <91268999+NewRenTL@users.noreply.github.com>
2024-01-05 19:28:55 -0500

    I'm exporting my java files where I'm working my class about Class Inheritance OOP

---
+++ src/Classes/LibraryItem.java
@@ -0,0 +1,38 @@
+package Classes;
+
+public class LibraryItem {
+    protected int id;
+    protected String title;
+
+    protected boolean available;
+
+    public LibraryItem(int id, String title, boolean available) {
+        this.id = id;
+        this.title = title;
+        this.available = available;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+}
